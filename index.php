0217d50282b7ba6a7c7f35240cba98f485b393b3
camcam1604 <camille.parisot@my-digital-school.org>
2024-06-07 08:57:46 +0200

    commit

--- index.php
+++ index.php
@@ -4,13 +4,29 @@
 <head>
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World</title>
+    <title>ToDo</title>
 
     <head>
 
     <body>
-        <h1>Hello World</h1>
-        <p> test</p>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <ul>
+                    <li class="nav-item"><a class="nav-link" href="index.php">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="create.php">Créer une liste</a></li>
+                    <li class="nav-item"><a class="nav-link" href="list.php">Mes listes</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <section class="container">
+                <h1>To Do List</h1>
+                <p>Créer ta propre to do liste</p>
+            </section>
+
+        </main>    
+        
+        
     </body>
 
 </html>
